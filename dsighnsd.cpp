44aba22656e196b8fed0fad6a3d5e567b1a4eb12
lcczxcvbnm <3385471983@qq.com>
2026-02-13 13:50:11 +0800

    1111

--- dsighnsd.cpp
+++ dsighnsd.cpp
@@ -1,6 +1,10 @@
 #include<stdio.h>
-intuyu (){
-    
-    print("123.com")
+int main (){
+    printf("hello world");
+    int a, b;
+        a = 10;
+        b = 20;
+        c=25;
+        printf("a + b+c = %d", a + b+c);
     return 0;
 }
