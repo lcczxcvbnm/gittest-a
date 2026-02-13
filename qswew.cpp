d1134ae61c91d7e4eac8ef3c8142fdd929e743ef
lcczxcvbnm <3385471983@qq.com>
2026-02-13 16:45:02 +0800

    789

---
+++ qswew.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main (){
+    printf("hello world");
+    int a, b;
+        a = 10;
+        b = 20;
+        c=25;
+        printf("a + b+c = %d", a + b+c);
+    return 0;
+}
