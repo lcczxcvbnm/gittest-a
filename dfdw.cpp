8cead7d4dcacea1a9c226b7282a4b1c81c6b0a48
lcczxcvbnm <3385471983@qq.com>
2026-03-07 10:45:21 +0800

    change2

--- dfdw.cpp
+++ dfdw.cpp
@@ -7,5 +7,7 @@ int main (){
         printf("a + b = %d", a + b);
         printf("a + b +60= %d", a + b+60);
         printf("a + b+10= %d", a + b+10);
+          printf("a + b+10= %d", a + b+1020);
+
     return 0;
 }
